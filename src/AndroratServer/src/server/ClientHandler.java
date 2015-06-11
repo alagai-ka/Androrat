e808e341fd33de059104c63be112faa27a92eaf4
alagai-ka <alagai_ka@web.de>
2015-06-11 15:52:58 +0200

    Comments on: ClientHandler

--- src/AndroratServer/src/server/ClientHandler.java
+++ src/AndroratServer/src/server/ClientHandler.java
@@ -16,17 +16,56 @@ import in.Demux;
 import in.Receiver;
 import out.Mux;
 
+/**
+ * Diese Klasse ist für das empfangen der Daten auf dem Server zuständig.
+ */
 public class ClientHandler extends Thread {
+	/**
+	 * imei	Die IMEI des Gerätes
+	 */
 	private String imei;
+	/**
+	 * clientSocket Der Socket des Gerätes mit der imei
+	 */
 	private Socket clientSocket;
+	/**
+	 * receiver	Der Empfänger der Daten
+	 */
 	private Receiver receiver;
+	/**
+	 * server	Der Server
+	 */
 	private Server server;
+	/**
+	 * demux	Der Demultiplexer
+	 */
 	private Demux demux;
+	/**
+	 * mux	Der Multiplexer
+	 */
 	private Mux mux;
+	/**
+	 * buffer	In diesem Buffer werden die Daten gespeichert.
+	 */
 	private ByteBuffer buffer;
+	/**
+	 * connected	Hier wird festgehalten ob eine Verbindung zum Client besteht.
+	 */
 	private boolean connected;
+	/**
+	 * mainGUI	Die Gui
+	 */
 	private GUI mainGUI;
 
+	/**
+	 * Der Konstruktor weißt den Klassenvariablen die übergebenen Daten zu.
+	 * Zusätzlich werden mit den übergebenen Daten auch die benötigten Objekte erstellt.
+	 * @param your_socket	Der Socket
+	 * @param id	Die id
+	 * @param s	Der Server
+	 * @param mainGUI	Die HUI
+	 * @throws IOException
+	 */
 	public ClientHandler(Socket your_socket, String id, Server s, GUI mainGUI)
 			throws IOException {
 		this.mainGUI = mainGUI;
@@ -43,7 +82,15 @@ public class ClientHandler extends Thread {
 	}
 
 	// attend des donn�es du Receiver et les transmet au Demultiplexeur
+
+	/**
+	 * Diese Methode empfängt die Daten und übergibt diese dem Demultiplexer. Zusätzlich werden hier verschiedene Fehler abgefangen,
+	 * die dazu führen, dass die Verbindung entfernt wird.
+	 */
 	public void run() {
+		/**
+		 * Die Schleife ist solange aktiv wie es eine bestehende Verbindung gibt.
+		 */
 		while (connected) {
 
 			try {
@@ -51,6 +98,9 @@ public class ClientHandler extends Thread {
 			     //System.out.println("");
 				
 				//buffer = receiver.read();
+				/**
+				 * Speichert die Daten des Receivers in dem Buffer
+				 */
 			     buffer = receiver.read(buffer);
 			     
 				try {
@@ -59,6 +109,9 @@ public class ClientHandler extends Thread {
 						buffer.compact();
 					}
 				} catch (Exception e) {
+					/**
+					 * Es gab ein Fehler bei dem Demultiplexer. Daher wird eine Lognachricht erstellt und die Schleife beendet.
+					 */
 					connected = false;
 					/*
 					connected = false;
@@ -73,9 +126,14 @@ public class ClientHandler extends Thread {
 
 			}
 			catch (IOException e) {
+				/**
+				 * Beenden der Schleife
+				 */
 				connected = false;
 				try {
-					
+					/**
+					 * Der Socket wird geschlossen und der User mit der IMEI von der GUI gelöscht.
+					 */
 					clientSocket.close();
 					mainGUI.deleteUser(imei);
 				} catch (IOException e1) {
@@ -83,9 +141,15 @@ public class ClientHandler extends Thread {
 				}
 			}
 			catch(IndexOutOfBoundsException e) {
+				/**
+				 * Lognachricht erstellen und die Schleife beenden
+				 */
 				server.getGui().logErrTxt("Client ended gently !");
 				connected = false;
 				try {
+					/**
+					 * Socket wird geschlossen und das Gerät von der Gui entfernt.
+					 */
 					clientSocket.close();
 					mainGUI.deleteUser(imei);
 				} catch (IOException e1) {
@@ -93,10 +157,20 @@ public class ClientHandler extends Thread {
 				}
 			}
 		}
+		/**
+		 * Der Handler wird gelöscht.
+		 */
 		server.DeleteClientHandler(imei);
 	}
 
 	// transmet les donn�es � envoyer au Multiplexeur
+
+	/**
+	 * Diese Methode erstellt ein neues CommandPacket und gibt dies der Klasse mux zum Versenden.
+	 * @param command	Der Befehl
+	 * @param channel	Der Kanal
+	 * @param args	Die Argumente
+	 */
 	public void toMux(short command, int channel, byte[] args) {
 		Packet packet = new CommandPacket(command, channel, args);
 		mux.send(0, packet.build());
@@ -106,6 +180,10 @@ public class ClientHandler extends Thread {
 
 	}
 
+	/**
+	 * Eine neue IMEI erhalten und setze.
+	 * @param i	Die neue IMEI
+	 */
 	public void updateIMEI(String i) {
 		imei = i;
 		demux.setImei(imei);
