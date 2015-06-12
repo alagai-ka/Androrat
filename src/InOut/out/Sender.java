9b383f736fd389da04989c65163b5f6fae4b82ef
alagai-ka <alagai_ka@web.de>
2015-06-12 15:54:36 +0200

    Comments on: Sender

--- src/InOut/out/Sender.java
+++ src/InOut/out/Sender.java
@@ -3,15 +3,28 @@ package out;
 import java.io.DataOutputStream;
 import java.io.IOException;
 
+/**
+ * Diese Methode ist zum Schreiben von Daten auf den DataOutputStream zuständig.
+ */
 public class Sender {
-
+	/**
+	 * out	Der DataOutputStream
+	 */
 	DataOutputStream out;
-	
+
+	/**
+	 * Weißt der Klassenvariable den übergebenen Stream zu.
+	 * @param out	Der DataoutputStream
+	 */
 	public Sender(DataOutputStream out)
 	{
 		this.out = out ;
 	}
 
+	/**
+	 * Versucht die übergebenen Daten auf den Stream zu schreiben.
+	 * @param data	Die Daten
+	 */
 	public void send(byte[] data)
 	{
 		try
