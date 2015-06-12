7ebe1d67bd5e97cad886029d969961e4cb25b9d2
alagai-ka <alagai_ka@web.de>
2015-06-12 15:41:56 +0200

    Comments on: Demux und Receiver

--- src/InOut/in/Demux.java
+++ src/InOut/in/Demux.java
@@ -5,23 +5,43 @@ import inout.Controler;
 import inout.Protocol;
 import java.nio.ByteBuffer;
 
+/**
+ *
+ */
 public class Demux {
 	// acces au controler
+	/**
+	 * controler	Der Zugang zum Controler
+	 */
 	private Controler controler;
 
 	// un packet
+	/**
+	 * p	Ein Paket
+	 */
 	private TransportPacket p;
 
 
 	// l'identifiant du client
+	/**
+	 * imei	Die Imei des Clients
+	 */
 	private String imei;
 
 	// le buffer de lecture
+	/**
+	 * buffer	Ein Zwischenspeicher
+	 */
 	private ByteBuffer buffer;
 
 	// variables de controle
 	private boolean partialDataExpected, reading;
 
+	/**
+	 * Der Konstruktor weißt den Klassenvariablen die übergebenen Daten zu
+	 * @param s	Der Controler
+	 * @param i	Die IMEI
+	 */
 	public Demux(Controler s, String i) {
 		imei = i;
 		controler = s;
@@ -34,33 +54,50 @@ public class Demux {
 	{
 
 		while (reading) {
-			
-	
+
+				/**
+			 	* Überprüfen ob es die Daten partiell ankommen oder im gesamten.
+			 	*/
 				if(!partialDataExpected)
 					//si on n'attend pas de donn�es partielles(dans le cas d'un paquet pas re�ue enti�rement)
 				{	
 						// si la taille du buffer est insuffisante
+						/**
+					 	* Überprüfen ob die Buffer groß genug ist für die Daten.
+					 	*/
 						if ((buffer.limit() - buffer.position()) < Protocol.HEADER_LENGTH_DATA) 
 						{
-							
+							/**
+							 * True zurückgeben
+							 */
 							return true;
 						}
 				}
 	
 				// dans le cas d'un paquet partiellement recue
+				/**
+			 	* Wenn die Daten partiell ankommen so wird die parseCompleter Methode aufgerufen um die Daten in der Klassenvariablen des Pakets zu speichern.
+			 	*/
 				if (partialDataExpected)
 					partialDataExpected = p.parseCompleter(buffer);
-				else 
+				else
+				/**
+				 * Ansonsten wird die parse-Methode  auf dem Paket p aufgerufen. Um herauszufinden ob immernoch nicht alle Daten angekommen sind.
+				 */
 				{
 					p = new TransportPacket();
 					partialDataExpected = p.parse(buffer);
 				}
-				
-				
-				
+
+				/**
+			 	* Wenn nach der Überprüfung des Pakets immer noch Daten erwartet werden diese also nicht komplett sind so wird true zurückgeliefert.
+			 	*/
 				if (partialDataExpected)
 					return true;
 				else
+				/**
+				 * Ansonsten wird die Storage Methode aufgerufen um die empfangenen Daten abzurufen und zu speichern.
+				 */
 					controler.Storage(p, imei);
 			
 		}
@@ -70,6 +107,10 @@ public class Demux {
 		return true;
 	}
 
+	/**
+	 * Diese Methode setzt die IMEI
+	 * @param i	Die IMEI
+	 */
 	public void setImei(String i) {
 		imei = i;
 	}
--- src/InOut/in/Receiver.java
+++ src/InOut/in/Receiver.java
@@ -11,13 +11,34 @@ import java.util.concurrent.LinkedBlockingQueue;
 
 import inout.Protocol;
 
+/**
+ * Diese Klasse ist zum Empfangen von Daten gedacht.
+ */
 public class Receiver {
-
+	/**
+	 * socket	Der Softwaresocket
+	 */
 	private Socket socket;
+	/**
+	 * received_data	Die empfangenen Daten
+	 */
 	private byte[] received_data;
+	/**
+	 * buffer	Ein Zwischenspeicher
+	 */
 	private ByteBuffer buffer;
+	/**
+	 * is	Ein Stream
+	 */
 	private InputStream is;
 
+	/**
+	 * Dies ist der Konstruktor. Er weißt den übergebenen Socket der Klassenvariablen zu.
+	 * Danach wird für diesen Socket ein neuer InputStream erstellt und in der Klassenvariablen gespeichert.
+	 * Danach werden die beiden Buffer mit der MAX_PACKET_SIZE erstellt.
+	 * @param s	Der Socket
+	 * @throws IOException
+	 */
 	public Receiver(Socket s) throws IOException {
 		socket = s;
 		is = socket.getInputStream();
@@ -26,6 +47,14 @@ public class Receiver {
 		buffer = ByteBuffer.allocate(Protocol.MAX_PACKET_SIZE);
 	}
 
+	/**
+	 * Liest die Bytes der Klassenvariablen received_data und speichert die Anzahl der gelesenen Bytes in n.
+	 * Danach wird der buffer geleert. Im Anschluss werden die n Bytes von received_data in dem buffer gespeichert.
+	 * Danach wird dieser Buffer zurückgegeben.
+	 * @return	Der Buffer der die reveiced_data enthält.
+	 * @throws IOException
+	 * @throws SocketException
+	 */
 	public ByteBuffer read() throws IOException, SocketException { // A supprimer !
 		int n = 0;
 
@@ -37,33 +66,65 @@ public class Receiver {
 
 		return buffer;
 	}
-	
+
+	/**
+	 * Erstellt received_data aus dem ByteBuffer.
+	 * @param b	Der ByteBuffer
+	 * @return	Der Buffer mit den received Daten.
+	 * @throws IOException
+	 * @throws SocketException
+	 */
 	public ByteBuffer read(ByteBuffer b) throws IOException, SocketException {
 		int n = 0;
 		
 		byte[] theRest = null;
-		
+		/**
+		 * Überprüfen ob die Position des ByteBuffers größer als 0 und kleiner als HEADER_LENGTH_DATA ist.
+		 */
 		if(b.position()>0 && b.position()<Protocol.HEADER_LENGTH_DATA)
 		{
+			/**
+			 * Erstellt ein Byte-Array das so groß ist wie die Position des Buffers ist.
+			 */
 			theRest = new byte[b.position()];
 			b.flip();
+			/**
+			 * Hier werden die Daten von b bis zur BufferPosition in theRest gespeichert.
+			 */
 			b.get(theRest, 0, b.limit());
+			/**
+			 * Fügt die Daten aus theRest dem Buffer receivedData hinzu.
+			 */
 			System.arraycopy(theRest, 0, received_data, 0, theRest.length);
 			//for(int i = 0; i<theRest.length;i++)
 			//	received_data[i] = theRest[i];
 			
 			//System.out.println("theRest len = "+theRest.length);
+			/**
+			 * Liest die Daten von received_data, ab der Länge von theRest bis zum Max-Size-die Länge von the Rest.
+			 */
 			n = is.read(received_data,theRest.length,Protocol.MAX_PACKET_SIZE-theRest.length);
+			/**
+			 * Danach wird n um die Länge von the Rest erhöht.
+			 */
 			n+=theRest.length;
 		  }
 		else
+		/**
+		 * Liest die Daten von received_data und speichert die Zahl der gelesenen Bytes in n
+		 */
 			n = is.read(received_data);
 		
 		
-		//buffer.clear();		
+		//buffer.clear();
+		/**
+		 * Speichert received_data in dem buffer
+		 */
 		buffer = ByteBuffer.wrap(received_data, 0, n);
 		//System.out.println("data has been read:" + n);
-
+		/**
+		 * Gibt den Buffer zurück.
+		 */
 		return buffer;
 	}
 
